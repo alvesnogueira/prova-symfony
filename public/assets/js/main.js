6df3e07185d39ecb276d34c25ea4570cbacd6f03
vagrant <vagrant@machine1.puphpet>
2018-04-15 21:02:08 +0000

    create migrations

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,12 +1,24 @@
 $(document).ready(function() {
-var url = "assets/js/json-estados.json";
-    $.getJSON(url, function(json){
-        var $select_elem = $("#uf");
-        $select_elem.empty();
-        $.each(json, function (idx, obj) {
-            console.log(obj);
-            $select_elem.append('<option value="' + obj.Sigla + '">' + obj.Nome + '</option>');
-        });
-        $select_elem.chosen({ width: "95%" });
+// var url = "assets/js/json-estados.json";
+//     $.getJSON(url, function(json){
+//         var $select_elem = $("#uf");
+//         $select_elem.empty();
+//         $.each(json, function (idx, obj) {
+//             $select_elem.append('<option value="' + obj.Sigla + '">' + obj.Nome + '</option>');
+//         });
+//         $select_elem.chosen({ width: "95%" });
+// });
+
+var url = "http://www.geonames.org/childrenJSON?geonameId=3469034";
+$.getJSON(url, function(json){
+    var $select_elem = $("#uf");
+    $select_elem.empty();
+    $.each(json.geonames, function (idx, obj) {
+            console.log(obj.adminCodes1.ISO3166_2);
+            $select_elem.append('<option value="' + obj.adminCodes1.ISO3166_2 + '">' + obj.adminName1 + '</option>');
     });
+    $select_elem.chosen({ width: "95%" });
+});
+
+
 });
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,15 +1,19 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\User;
+
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-//use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends AbstractController
 {
 
     /**
      * @Route("/")
+     * @Method({"GET"})
      */
     public function homepage()
     {
@@ -20,6 +24,20 @@ class DefaultController extends AbstractController
        //      '<html><body>Lucky number: </body></html>'
        //  );
     }
-
-
+    /**
+     * @Route("/endereco/save")
+     */
+    public function save(){
+        $entityManager = $this->getDoctrine()->getManager();
+        $user = new User();
+        $user->setName('Israel');
+        $user->setUf('MG');
+        $user->setCidade('Belo Horizonte');
+        $user->setCep('31170560');
+        $user->setBairro('Uniao');
+        $user->setLogradouro('Rua Pedro Jaques');
+        $entityManager->persist($user);
+        $entityManager->flush();
+        return new Response("Salvo".$user->getId());
+    }
 }
