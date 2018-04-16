1a975dbc7475ee61e96f9b99020268b431847684
Israel Alves <israel.nogueira@gdax.com.br>
2018-04-16 00:19:17 -0300

    Litando dados

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,11 +4,12 @@ namespace App\Controller;
 use App\Entity\User;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends AbstractController
+class DefaultController extends Controller
 {
 
     /**
@@ -20,24 +21,89 @@ class DefaultController extends AbstractController
          return $this->render('index.html.twig', [
              'name' => 'teste',
          ]);
-       // return new Response(
-       //      '<html><body>Lucky number: </body></html>'
-       //  );
+    }
+    /**
+     * @Route("/listar")
+     * @Method({"GET"})
+     */
+    public function listaDados()
+    {
+        $users = $this->getDoctrine()
+                 ->getRepository(User::class)
+                 ->findAll();
+
+        return $this->render('lista.html.twig', [
+            'users' => $users,
+        ]);
+
     }
     /**
      * @Route("/endereco/save")
      */
-    public function save(){
+    public function recebeForm(Request $request){
+        $erros = [];
+        if($request->get('nome') == '')
+        {
+            $erros['nome'] = 'Informe o nome';
+        }
+        if($request->get('uf') == '')
+        {
+            $erros['uf'] = 'Informe a UF';
+        }
+        if($request->get('cidade') == '')
+        {
+            $erros['cidade'] = 'Informe a cidade';
+        }
+        if($request->get('cep') == '')
+        {
+            $erros['cep'] = 'Informe o bairro';
+        }
+
+        if(count($erros) > 0)
+        {
+            $this->addFlash('notice','Preencha todos os campos obrigatorios');
+        }
+        else
+        {
+            $dados = [];
+            $cep = str_replace(array('-', '.', ' ','_', '(', ')'), '', $request->get('cep'));
+            $dados['nome'] = $request->get('nome');
+            $dados['uf'] = $request->get('uf');
+            $dados['cidade'] = $request->get('cidade');
+            $dados['cep'] = $cep;
+            $dados['bairro'] = $request->get('bairro');
+            $dados['logradouro'] = $request->get('logradouro');
+            $retorno = $this->saveEndereco($dados);
+            if($retorno)
+            {
+                $this->addFlash('notice', 'Endereço cadastrado com sucesso');
+            }
+            else
+            {
+                $this->addFlash('notice', 'Algo deu errado, por favor tente novamente mais tarde.');
+            }
+        }
+
+
+        //return $this->redirect($this->generateUrl('vo_show', array('id' => $entity->getId())));
+        return $this->redirect('/');
+
+    }
+
+    /**
+     *
+     */
+    private function saveEndereco($dados){
         $entityManager = $this->getDoctrine()->getManager();
         $user = new User();
-        $user->setName('Israel');
-        $user->setUf('MG');
-        $user->setCidade('Belo Horizonte');
-        $user->setCep('31170560');
-        $user->setBairro('Uniao');
-        $user->setLogradouro('Rua Pedro Jaques');
+        $user->setName($dados['nome']);
+        $user->setUf($dados['uf']);
+        $user->setCidade($dados['cidade']);
+        $user->setCep($dados['cep']);
+        $user->setBairro($dados['bairro']);
+        $user->setLogradouro($dados['logradouro']);
         $entityManager->persist($user);
         $entityManager->flush();
-        return new Response("Salvo".$user->getId());
+        return $user->getId();
     }
 }
